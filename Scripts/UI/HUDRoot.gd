236beae3f11f0791bd86bf522b9e8fe5e0f48c23
MajorBe <jotyre3@gmail.com>
2025-10-16 13:48:11 +0700

    Delete Scripts directory

--- Scripts/UI/HUDRoot.gd
+++
@@ -1,6 +0,0 @@
-extends CanvasLayer
-@onready var hp_bar: Control = $HPBar
-
-func set_health(cur: int, max_h: int) -> void:
-	if hp_bar and hp_bar.has_method("set_health"):
-		hp_bar.set_health(cur, max_h)
